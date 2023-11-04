9f44d0aad07698d749b7509faa5fff9a1487fb7f
Guilherme Lima e Silva <guilhermelimaesilva.p@gmail.com>
2023-11-04 11:32:16 -0300

    Terminando Desafio via cep (react)

--- carrinho_de_compra/src/App.jsx
+++ carrinho_de_compra/src/App.jsx
@@ -1,35 +1,104 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cep, setCep] = useState("");
+  const [endereco, setEndereco] = useState("");
+  const [numero, setNumero] = useState("");
+  const [bairro, setBairro] = useState("");
+  const [cidade, setCidade] = useState("");
+  const [estado, setEstado] = useState("");
+
+  const consultar_cep = () => {
+    const url = `https://viacep.com.br/ws/${cep}/json/`;
+
+    fetch(url)
+    .then((resultado) => resultado.json())
+    .then((data) => {
+      setCidade(data.localidade);
+      setEndereco(data.logradouro)
+      setBairro(data.bairro)
+      setEstado(data.uf)
+    });
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <section className="row mt-5 align-items-center">
+          <div className="col-6">
+            <img
+              className="img-fluid"
+              src="./undraw_meet_the_team_re_4h08.svg"
+              alt=""
+            />
+          </div>
+
+          <div className="col-6 data-bs-theme bg-white rounded ">
+            <form className="mb-md-5">
+              <label htmlFor="cep">CEP</label>
+              <input
+                id="cep"
+                type="text"
+                className="form-control mb-3"
+                value={cep}
+                onChange={(e) => setCep(e.target.value)}
+                onBlur={consultar_cep}
+              />
+
+              <label htmlFor="endereco">Endereço</label>
+              <input
+                id="endereco"
+                type="text"
+                className="form-control mb-3"
+                value={endereco}
+                onChange={(e) => setEndereco(e.target.value)}
+              />
+
+              <label htmlFor="numero">Número</label>
+              <input
+                id="numero"
+                type="text"
+                className="form-control mb-3"
+                value={numero}
+                onChange={(e) => setNumero(e.target.value)}
+              />
+
+              <label htmlFor="bairro">Bairro</label>
+              <input
+                id="bairro"
+                type="text"
+                className="form-control mb-3"
+                value={bairro}
+                onChange={(e) => setBairro(e.target.value)}
+              />
+
+              <label htmlFor="cidade">Cidade</label>
+              <input
+                id="cidade"
+                type="text"
+                className="form-control mb-3"
+                value={cidade}
+                onChange={(e) => setCidade(e.target.value)}
+              />
+
+              <label htmlFor="estado">Estado</label>
+              <input
+                id="estado"
+                type="text"
+                className="form-control mb-3"
+                value={estado}
+                onChange={(e) => setEstado(e.target.value)}
+              />
+              <div className="d-grid">
+                <button className="btn btn-primary">ENVIAR</button>
+              </div>
+            </form>
+          </div>
+        </section>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
